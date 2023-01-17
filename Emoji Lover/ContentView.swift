77cf1223aaaec26af9d2d53e63444381687438a7
Renaro Santos <pryscilla01@gmail.com>
2023-01-17 17:20:29 +0100

    Initializing repository

---
+++ Emoji Lover/ContentView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+enum Emoji: String, CaseIterable {
+    case 📚, 🍎, 👩🏻‍🏫, 🏫
+}
+
+struct ContentView: View {
+    @State var selection: Emoji = .📚
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+        
+              
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Hey Pryscilla!")
+            .padding()
+        }
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Emoji Lover/Emoji_LoverApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct Emoji_LoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
